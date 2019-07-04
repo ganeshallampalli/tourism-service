f8d6b7fa3a5b59fe9cef2830d4785f470d5b9539
ganeshallampalli <ganeshallampalli@gmail.com>
2019-07-04 21:41:50 +0530

    Generation type set to identity

--- src/main/java/com/tourism/datamodel/Cruise.java
+++ src/main/java/com/tourism/datamodel/Cruise.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 public class Cruise {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String name;
